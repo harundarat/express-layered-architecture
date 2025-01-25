728e4b4308c06efb0e20428430df37952149091c
Harun Al Rasyid <harundarat@gmail.com>
2025-01-25 11:10:57 +0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const cors require("cors");
+const cors = require("cors");
 const port = process.env.PORT;
 const prisma = require("./config/database");
 const routes = require("./routes");
